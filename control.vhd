59c85d37fe728d3563aa9c3800f85899f89ed056
@E440QF <ettore.beltra@gmail.com>
2024-12-22 20:48:05 +0100

    palele 2

--- control.vhd
+++ control.vhd
@@ -5,7 +5,8 @@ use work.common_const.all;
 
 entity control_unit is
     port(
-        clk     : in std_logic;
-        opcode  : in std_logic_vector(3 downto 0);
+        clk         : in std_logic;
+        opcode      : in std_logic_vector(3 downto 0);
+        control_out : out std_logic_vector(5 downto 0);
 
         )
