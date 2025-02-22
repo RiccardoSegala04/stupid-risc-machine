f6b6cbc661d0c5c7cd73f4ebe56d0b15ccf923ac
RiccardoSegala04 <riccardo.segala@icloud.com>
2025-02-22 22:37:45 +0100

    Change cpu opset

--- entities/common_const.vhd
+++ entities/common_const.vhd
@@ -14,18 +14,18 @@ package common_const is
     constant OP_SBC      : std_logic_vector(3 downto 0) := "0001";
     constant OP_AND      : std_logic_vector(3 downto 0) := "0010";
     constant OP_OR       : std_logic_vector(3 downto 0) := "0011";
-    constant OP_NOT      : std_logic_vector(3 downto 0) := "0100";
-    constant OP_SL       : std_logic_vector(3 downto 0) := "0101";
-    constant OP_SR       : std_logic_vector(3 downto 0) := "0110";
-    constant OP_CMP      : std_logic_vector(3 downto 0) := "0111";
-    constant OP_MOV      : std_logic_vector(3 downto 0) := "1000";
-    constant OP_JEQ_REL  : std_logic_vector(3 downto 0) := "1001";
-    constant OP_JGT_REL  : std_logic_vector(3 downto 0) := "1010";
-    constant OP_JLT_REL  : std_logic_vector(3 downto 0) := "1011";
-    constant OP_LOAD_MEM : std_logic_vector(3 downto 0) := "1100";
-    constant OP_LOAD_IMM : std_logic_vector(3 downto 0) := "1101";
-    constant OP_STORE    : std_logic_vector(3 downto 0) := "1110";
-    constant OP_NOP      : std_logic_vector(3 downto 0) := "1111";
+    constant OP_XOR      : std_logic_vector(3 downto 0) := "0100";
+    constant OP_NOT      : std_logic_vector(3 downto 0) := "0101";
+    constant OP_SL       : std_logic_vector(3 downto 0) := "0110";
+    constant OP_SR       : std_logic_vector(3 downto 0) := "0111";
+    constant OP_CMP      : std_logic_vector(3 downto 0) := "1000";
+    constant OP_MOV      : std_logic_vector(3 downto 0) := "1001";
+    constant OP_JEQ_REL  : std_logic_vector(3 downto 0) := "1010";
+    constant OP_JGT_REL  : std_logic_vector(3 downto 0) := "1011";
+    constant OP_JLT_REL  : std_logic_vector(3 downto 0) := "1100";
+    constant OP_LOAD_MEM : std_logic_vector(3 downto 0) := "1101";
+    constant OP_LOAD_IMM : std_logic_vector(3 downto 0) := "1110";
+    constant OP_STORE    : std_logic_vector(3 downto 0) := "1111";
 
     -- ALU OPCODES
     constant ALU_ADC : std_logic_vector(2 downto 0) := "000";
